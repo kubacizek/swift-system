79a56cc2a01d857a50a049d5a06abc0be5d77739
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-07 07:56:32 -0500

    Updated `NetlinkProtocol`

--- Sources/System/SocketProtocol.swift
+++ Sources/System/SocketProtocol.swift
@@ -24,8 +24,10 @@ public enum UnixProtocol: Int32, Codable, SocketProtocol {
     
     case raw = 0
     
+    @_alwaysEmitIntoClient
     public static var family: SocketAddressFamily { .unix }
     
+    @_alwaysEmitIntoClient
     public var type: SocketType {
         switch self {
         case .raw: return .raw
@@ -40,8 +42,10 @@ public enum IPv4Protocol: Int32, Codable, SocketProtocol {
     case tcp
     case udp
     
+    @_alwaysEmitIntoClient
     public static var family: SocketAddressFamily { .ipv4 }
     
+    @_alwaysEmitIntoClient
     public var type: SocketType {
         switch self {
         case .raw: return .raw
@@ -50,6 +54,7 @@ public enum IPv4Protocol: Int32, Codable, SocketProtocol {
         }
     }
     
+    @_alwaysEmitIntoClient
     public var rawValue: Int32 {
         switch self {
         case .raw: return _IPPROTO_RAW
@@ -66,8 +71,10 @@ public enum IPv6Protocol: Int32, Codable, SocketProtocol {
     case tcp
     case udp
     
+    @_alwaysEmitIntoClient
     public static var family: SocketAddressFamily { .ipv6 }
     
+    @_alwaysEmitIntoClient
     public var type: SocketType {
         switch self {
         case .raw: return .raw
@@ -76,6 +83,7 @@ public enum IPv6Protocol: Int32, Codable, SocketProtocol {
         }
     }
     
+    @_alwaysEmitIntoClient
     public var rawValue: Int32 {
         switch self {
         case .raw: return _IPPROTO_RAW
@@ -113,8 +121,10 @@ public enum BluetoothProtocol: Int32, Codable, SocketProtocol {
     /// Audio/video data transport protocol
     case avdtp      = 7
     
+    @_alwaysEmitIntoClient
     public static var family: SocketAddressFamily { .bluetooth }
     
+    @_alwaysEmitIntoClient
     public var type: SocketType {
         switch self {
         case .l2cap:    return .sequencedPacket
@@ -133,16 +143,88 @@ public enum BluetoothProtocol: Int32, Codable, SocketProtocol {
 public enum NetLinkProtocol: Int32, Codable, SocketProtocol {
         
     case generic
+    
+    /* Receives routing and link updates and may be used to
+     modify the routing tables (both IPv4 and IPv6), IP
+     addresses, link parameters, neighbor setups, queueing
+     disciplines, traffic classes, and packet classifiers. */
     case route
+    
+    /// Messages from 1-wire subsystem.
+    case wire1
+    
+    /// Reserved for user-mode socket protocols.
+    case user
+    
+    /* Transport IPv4 packets from netfilter to user space.  Used
+     by ip_queue kernel module.  After a long period of being
+     declared obsolete (in favor of the more advanced
+     nfnetlink_queue feature), NETLINK_FIREWALL was removed in
+     Linux 3.5. */
+    case firewall
+    
+    /// Query information about sockets of various protocol families from the kernel.
+    case socketDiagnosis
+    
+    /// Netfilter/iptables ULOG.
+    case log
+    
+    /// IPsec
+    case xfrm
+    
+    /// SELinux event notifications.
+    case selinux
+    
+    /// Open-iSCSI.
+    case iscsi
+    
+    /// Auditing.
+    case audit
+    
+    /// Access to FIB lookup from user space.
+    case fibLookup
+    
+    /// Kernel connector.
+    case connector
+    
+    /// Netfilter subsystem.
+    case netfilter
+    
+    /// SCSI Transports.
+    case scsiTransports
+    
+    /// Infiniband RDMA.
+    case rdma
+    
+    /// Transport IPv6 packets from netfilter to user space.
+    case ipv6Forward
+    
+    /// DECnet routing messages.
+    case decnetRoute
+    
+    /// Kernel messages to user space.
+    case uevent
+    
+    /// Netlink interface to request information about ciphers
+    /// registered with the kernel crypto API as well as allow
+    /// configuration of the kernel crypto API.
+    case crypto
     
     public static var family: SocketAddressFamily { .netlink }
     
-    public var type: SocketType { return .raw }
+    public var type: SocketType {
+        /* Netlink is a datagram-oriented service.  Both SOCK_RAW and
+         SOCK_DGRAM are valid values for socket_type.  However, the
+         netlink protocol does not distinguish between datagram and raw
+         sockets.*/
+        return .raw
+    }
     
+    @_alwaysEmitIntoClient
     public var rawValue: Int32 {
         switch self {
-        case .generic: return NETLINK_GENERIC
-        case .route: return NETLINK_ROUTE
+        case .generic: return _NETLINK_GENERIC
+        case .route: return _NETLINK_ROUTE
         }
     }
 }
