3ab59e5b118e4ef4e9f143b4f743ff2a9d988079
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-07 09:43:32 -0500

    Added `IPv4Address`

--- Sources/System/SocketAddress.swift
+++ Sources/System/SocketAddress.swift
@@ -16,3 +16,52 @@ public protocol SocketAddress {
     func withUnsafeBytes<Result>(_ body: ((UnsafeRawBufferPointer) -> (Result))) -> Result
 }
 
+/// IPv4 Socket Address
+public struct IPv4Address: SocketAddress, Equatable, Hashable {
+    
+    @usableFromInline
+    internal let bytes: CInterop.IPv4Address
+    
+    @_alwaysEmitIntoClient
+    internal init(_ bytes: CInterop.IPv4Address) {
+        self.bytes = bytes
+    }
+    
+    @_alwaysEmitIntoClient
+    public static var family: SocketAddressFamily { .ipv4 }
+    
+    @_alwaysEmitIntoClient
+    public func withUnsafeBytes<Result>(_ body: ((UnsafeRawBufferPointer) -> (Result))) -> Result {
+        Swift.withUnsafeBytes(of: bytes, body)
+    }
+}
+
+public extension IPv4Address {
+    
+    @_alwaysEmitIntoClient
+    static var any: IPv4Address { IPv4Address(_INADDR_ANY) }
+}
+
+extension IPv4Address: RawRepresentable {
+    
+    @_alwaysEmitIntoClient
+    public init?(rawValue: String) {
+        guard let bytes = try? CInterop.IPv4Address(rawValue) else {
+            return nil
+        }
+        self.init(bytes)
+    }
+    
+    @_alwaysEmitIntoClient
+    public var rawValue: String {
+        return try! String(bytes)
+    }
+}
+
+extension IPv4Address: CustomStringConvertible {
+    
+    @_alwaysEmitIntoClient
+    public var description: String {
+        return rawValue
+    }
+}
