c80056a8eea468791aa00f5bafccd8b22ea86182
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-08 15:08:17 -0500

    Added `IPv4SocketAddress`

--- Sources/System/SocketAddress.swift
+++ Sources/System/SocketAddress.swift
@@ -13,6 +13,63 @@ public protocol SocketAddress {
     /// Socket Address Family
     static var family: SocketAddressFamily { get }
     
-    func withUnsafeBytes<Result>(_ body: ((UnsafeRawBufferPointer) -> (Result))) -> Result
+    func withUnsafePointer<Result>(_ body: ((UnsafePointer<CInterop.SocketAddress>) -> (Result))) -> Result
 }
 
+/// IPv4 Socket Address
+public struct IPv4SocketAddress: SocketAddress, Equatable, Hashable {
+    
+    @_alwaysEmitIntoClient
+    public static var family: SocketAddressFamily { .ipv4 }
+    
+    @_alwaysEmitIntoClient
+    public var address: IPv4Address
+    
+    public var port: UInt16
+    
+    public init(address: IPv4Address,
+                port: UInt16) {
+        
+        self.address = address
+        self.port = port
+    }
+    
+    public func withUnsafePointer<Result>(_ body: ((UnsafePointer<CInterop.SocketAddress>) -> (Result))) -> Result {
+        
+        var socketAddress = CInterop.IPv4SocketAddress()
+        socketAddress.sin_family = numericCast(Self.family.rawValue)
+        socketAddress.sin_port = port.bigEndian
+        socketAddress.sin_addr = address.bytes
+        socketAddress.sin_len = 0
+        assert(MemoryLayout.size(ofValue: socketAddress) == MemoryLayout<CInterop.SocketAddress>.size)
+        return Swift.withUnsafePointer(to: unsafeBitCast(socketAddress, to: CInterop.SocketAddress.self), body)
+    }
+}
+
+/// IPv6 Socket Address
+public struct IPv6SocketAddress: SocketAddress, Equatable, Hashable {
+    
+    public static var family: SocketAddressFamily { .ipv6 }
+    
+    public var address: IPv6Address
+    
+    public var port: UInt16
+    
+    public init(address: IPv6Address,
+                port: UInt16) {
+        
+        self.address = address
+        self.port = port
+    }
+    
+    public func withUnsafePointer<Result>(_ body: ((UnsafePointer<CInterop.SocketAddress>) -> (Result))) -> Result {
+        
+        var socketAddress = CInterop.IPv6SocketAddress()
+        socketAddress.sin6_family = numericCast(Self.family.rawValue)
+        socketAddress.sin6_port = port.bigEndian
+        socketAddress.sin6_addr = address.bytes
+        socketAddress.sin6_len = 0
+        assert(MemoryLayout.size(ofValue: socketAddress) == MemoryLayout<CInterop.SocketAddress>.size)
+        return Swift.withUnsafePointer(to: unsafeBitCast(socketAddress, to: CInterop.SocketAddress.self), body)
+    }
+}
