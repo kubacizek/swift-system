1b5971baac608de09d8003648e855dc1514b7fc6
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-12 20:41:53 -0500

    Optimized `CInternetAddress`

--- Sources/System/Internals/CInternetAddress.swift
+++ Sources/System/Internals/CInternetAddress.swift
@@ -10,7 +10,7 @@
 @usableFromInline
 internal protocol CInternetAddress {
     
-    static var stringLength: UInt32 { get }
+    static var stringLength: Int { get }
     
     static var family: SocketAddressFamily { get }
     
@@ -21,19 +21,17 @@ internal extension CInternetAddress {
     
     @usableFromInline
     init(_ string: String) throws {
-        
+        self = try Self.parse(string).get()
+    }
+    
+    @usableFromInline
+    static func parse(_ string: String) -> Result<Self, Errno> {
         var address = Self.init()
-        
-        /**
-         inet_pton() returns 1 on success (network address was successfully converted). 0 is returned if src does not contain a character string representing a valid network address in the specified address family. If af does not contain a valid address family, -1 is returned and errno is set to EAFNOSUPPORT.
-        */
-        try nothingOrErrno(retryOnInterrupt: false) {
+        return nothingOrErrno(retryOnInterrupt: false) {
             string.withCString { cString in
                 system_inet_pton(Self.family.rawValue, cString, &address)
             }
-        }.get()
-        
-        self = address
+        }.flatMap { .success(address) }
     }
 }
 
@@ -41,27 +39,24 @@ internal extension String {
     
     @usableFromInline
     init<T: CInternetAddress>(_ cInternetAddress: T) throws {
-        
-        let cStringLength = T.stringLength
-        let cString = UnsafeMutablePointer<CInterop.PlatformChar>.allocate(capacity: Int(cStringLength))
-        defer { cString.deallocate() }
-        
-        let success = withUnsafePointer(to: cInternetAddress) { addressPointer in
-            system_inet_ntop(T.family.rawValue, addressPointer, cString, cStringLength) != nil
+        try self.init(_unsafeUninitializedCapacity: Int(T.stringLength)) { stringBuffer in
+            try stringBuffer.withMemoryRebound(to: CChar.self) { charBuffer in
+                let success = withUnsafePointer(to: cInternetAddress) { addressPointer in
+                    system_inet_ntop(T.family.rawValue, addressPointer, charBuffer.baseAddress!, UInt32(stringBuffer.count)) != nil
+                }
+                guard success else {
+                    throw Errno.current
+                }
+            }
+            return T.stringLength
         }
-        
-        guard success else {
-            throw Errno.current
-        }
-        
-        self.init(cString: cString)
     }
 }
 
 extension CInterop.IPv4Address: CInternetAddress {
     
     @usableFromInline
-    static var stringLength: UInt32 { return UInt32(_INET_ADDRSTRLEN) }
+    static var stringLength: Int { return numericCast(_INET_ADDRSTRLEN) }
     
     @usableFromInline
     static var family: SocketAddressFamily { .ipv4 }
@@ -70,7 +65,7 @@ extension CInterop.IPv4Address: CInternetAddress {
 extension CInterop.IPv6Address: CInternetAddress {
     
     @usableFromInline
-    static var stringLength: UInt32 { return UInt32(_INET6_ADDRSTRLEN) }
+    static var stringLength: Int { return numericCast(_INET6_ADDRSTRLEN) }
     
     @usableFromInline
     static var family: SocketAddressFamily { .ipv6 }
