233746169cc83e71f354b53aeddf0d2ab5feffb7
Alsey Coleman Miller <alseycmiller@gmail.com>
2021-10-21 06:34:07 -0500

    Added async `write()`

--- Sources/System/AsyncFileOperations.swift
+++ Sources/System/AsyncFileOperations.swift
@@ -8,7 +8,7 @@
 */
 
 #if swift(>=5.5)
-@available(iOS 15, *)
+@available(macOS 12, iOS 15, *)
 public extension FileDescriptor {
     
     /// Reads bytes at the current file offset into a buffer.
@@ -20,7 +20,7 @@ public extension FileDescriptor {
     ///     The default is `true`.
     ///     Pass `false` to try only once and throw an error upon interruption.
     ///   - sleepOnBlock: The number of nanoseconds to sleep if the operation
-    ///     throws ``Errno/wouldBlock`` or other async I/O errors..
+    ///     throws ``Errno/wouldBlock`` or other async I/O errors.
     /// - Returns: The number of bytes that were read.
     ///
     /// The <doc://com.apple.documentation/documentation/swift/unsafemutablerawbufferpointer/3019191-count> property of `buffer`
@@ -42,27 +42,57 @@ public extension FileDescriptor {
             _read(into: buffer, retryOnInterrupt: retryOnInterrupt)
         }.get()
     }
+    
+    /// Writes the contents of a buffer at the current file offset.
+    ///
+    /// - Parameters:
+    ///   - buffer: The region of memory that contains the data being written.
+    ///   - retryOnInterrupt: Whether to retry the write operation
+    ///     if it throws ``Errno/interrupted``.
+    ///     The default is `true`.
+    ///     Pass `false` to try only once and throw an error upon interruption.
+    ///   - sleepOnBlock: The number of nanoseconds to sleep if the operation
+    ///     throws ``Errno/wouldBlock`` or other async I/O errors.
+    /// - Returns: The number of bytes that were written.
+    ///
+    /// After writing,
+    /// this method increments the file's offset by the number of bytes written.
+    /// To change the file's offset,
+    /// call the ``seek(offset:from:)`` method.
+    ///
+    /// The corresponding C function is `write`.
+    @_alwaysEmitIntoClient
+    func write(
+      _ buffer: UnsafeRawBufferPointer,
+      retryOnInterrupt: Bool = true,
+      sleepOnBlock sleep: UInt64 = 1000
+    ) async throws -> Int {
+        try await retryOnBlock(sleep: sleep) {
+            _write(buffer, retryOnInterrupt: retryOnInterrupt)
+        }.get()
+    }
 }
 
-@available(iOS 15, *)
+@available(macOS 12, iOS 15, *)
 internal extension FileDescriptor {
     
-    /// Pauses the current task if the operation throws ``Errno/wouldBlock`` or other async I/O errors..
+    /// Pauses the current task if the operation throws ``Errno/wouldBlock`` or other async I/O errors.
     @usableFromInline
     func retryOnBlock<T>(
         sleep nanoseconds: UInt64,
         _ body: () -> Result<T, Errno>
     ) async throws -> Result<T, Errno> {
         repeat {
-          switch body() {
-          case let .success(result):
-              return .success(result)
-          case let .failure(error):
-              guard error.isBlocking else {
-                  return .failure(error)
-              }
-              try await Task.sleep(nanoseconds: nanoseconds)
-          }
+            try Task.checkCancellation()
+            switch body() {
+            case let .success(result):
+                return .success(result)
+            case let .failure(error):
+                guard error.isBlocking else {
+                    return .failure(error)
+                }
+                try await Task.sleep(nanoseconds: nanoseconds)
+            }
         } while true
     }
 }
@@ -81,5 +111,4 @@ internal extension Errno {
         }
     }
 }
-
 #endif
